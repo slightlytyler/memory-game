4d07a76bdf7b9a46e732e41effe565bd4bf09026
Tyler Martinez <slightlytyler@gmail.com>
2014-12-02 10:27:26 -0600

    audio fix for ios

--- src/js/plugins/audio-playback.js
+++ src/js/plugins/audio-playback.js
@@ -1,15 +1,32 @@
 function audioPlayback() {
-  $('#volume').bind( "click", function() {
+  if(getIOSVersion()) {
+    $('#volume').toggleClass('muted')
+
+    $('#volume').bind( "click", function() {
 
     $(this).toggleClass('muted');
 
-    if($(this).hasClass('muted')) {
-      $('audio').prop("volume",0);
-    } else {
-      $('audio').prop("volume",.7);
-    }
-  });
+      if($(this).hasClass('muted')) {
+        $('audio')[0].pause();
+      } else {
+        $('audio')[0].play();
+      }
+    });
+  } else {
+    $('#volume').bind( "click", function() {
+
+      $(this).toggleClass('muted');
+
+      if($(this).hasClass('muted')) {
+        $('audio').prop("volume",0);
+      } else {
+        $('audio').prop("volume",.7);
+      }
+    });
+  }
 }
+
+
 
 $(window).load(function(){
   $('audio').prop("volume",.7);
